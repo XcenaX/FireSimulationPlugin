be602e382250da0f91265a01a379df764e004610
Сайфулин Владислав Владимирович <saifulinvladislav@gmail.com>
2024-02-28 18:12:46 +0500

    Added smoke simulation logic for graph

--- Source/FireSimulation/Public/BuildingGraph.h
+++ Source/FireSimulation/Public/BuildingGraph.h
@@ -5,15 +5,81 @@
 #include "BuildingGraph.generated.h"
 
 UENUM(BlueprintType)
-enum class EDoorStatus : uint8
+enum class EConnectionStatus : uint8
 {
-    Closed      UMETA(DisplayName = "Closed"),
-    Open        UMETA(DisplayName = "Open"),
+    DoorClosed      UMETA(DisplayName = "Door Closed"),
+    DoorOpen        UMETA(DisplayName = "Door Open"),
     NoDoor      UMETA(DisplayName = "No Door")
 };
 
 USTRUCT(BlueprintType)
-struct FRoomNode
+struct FCalculatedParameters
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float A; // Характеристика схемы развития пожара
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float N; // Характеристика схемы развития пожара.
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float GasReleasePerMeterBurn; // Объем газа, выделяющегося при сгорании на единицу площади.
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float LimitGasDensity; // Предел плотности газовой среды.
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float LimitGasTemperature; // Предел температуры газовой среды.
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float LimitSmokeExtinctionCoefficient; // Предел коэффициента затухания дыма.
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float LimitVisibility; // Предел видимости в условиях пожара.
+
+    FCalculatedParameters()
+        : A(0.f), N(0.f), GasReleasePerMeterBurn(0.f), LimitGasDensity(0.f),
+        LimitGasTemperature(0.f), LimitSmokeExtinctionCoefficient(0.f), LimitVisibility(0.f)
+    {
+    }
+};
+
+USTRUCT(BlueprintType)
+struct FFireDynamicsParameters
+{
+    GENERATED_BODY()
+
+public:
+    // Количество выгоревшей массы
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float BurnedMass;
+
+    // Плотность газовой среды в помещении
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float GasDensity;
+
+    // Температура газовой среды в помещении
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float GasTemperature;
+
+    // Коэффициент затухания дыма
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float SmokeExtinctionCoefficient;
+
+    // Видимость в помещении
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Fire Dynamics")
+    float Visibility;
+
+    // Конструктор по умолчанию для инициализации значений
+    FFireDynamicsParameters()
+        : BurnedMass(0.f), GasDensity(0.f), GasTemperature(0.f),
+        SmokeExtinctionCoefficient(0.f), Visibility(0.f) {}
+};
+
+UCLASS(BlueprintType)
+class URoomNode : public UObject
 {
     GENERATED_BODY()
 
@@ -21,46 +87,91 @@ public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Room")
     int32 RoomID;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Room")
-    float Area;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    bool IsGasSource;                                                   // Коэффициент полноты сгорания
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Room")
-    TArray<int32> Neighbors;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float CombustionCompletenessCoefficient;                            // Коэффициент полноты сгорания
 
-    FRoomNode() : RoomID(-1), Area(0.0f) {}
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float HeatAbsorptionCoefficient;                                    // Коэффициент теплопоглощения
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float StartTemperature;                                             // Начальная температура
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float InitialGasDensity;                                            // Начальная плотность газовой среды в помещении
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float Cp;                                                           // Удельная теплоемкость при постоянном давлении
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Room")
+    float RoomVolume;                                                   // Объем помещения
+
+    URoomNode();
+
+    UFUNCTION(BlueprintCallable, Category = "Room")
+    FCalculatedParameters GetCalculatedParameters() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Room")
+    void AddToFireDynamicsHistory(const FFireDynamicsParameters& NewParams);
+
+    UFUNCTION(BlueprintCallable, Category = "Room")
+    TArray<FFireDynamicsParameters> GetFireDynamicsHistory() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Room")
+    FCalculatedParameters InitializeCalculatedParams();
+
+protected:
+    float heat_of_combustion_;                               // Теплота сгорания.
+    float linear_flame_speed_rate_;                          // Линейная скорость распространения пожара.
+    float specific_fuel_burn_rate_;                          // Удельная скорость выгорания горючей нагрузки.
+    float smoke_forming_ability_;
+    FCalculatedParameters calculated_params_;               // Рассчитанные параметры, основанные на начальных параметрах.
+    TArray<FFireDynamicsParameters> fire_dynamics_history_; // История изменений динамических параметров пожара.
 };
 
 USTRUCT(BlueprintType)
-struct FDoorEdge
+struct FGraphEdge
 {
     GENERATED_BODY()
 
 public:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Door")
-    int32 RoomAID;
+    int32 RoomStartID;
     
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Door")
-    int32 RoomBID;
+    int32 RoomEndID;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Door")
-    EDoorStatus DoorStatus;
+    EConnectionStatus ConnectionStatus;
 
-    FDoorEdge() : RoomAID(-1), RoomBID(-1), DoorStatus(EDoorStatus::Closed) {}
+    FGraphEdge() : RoomStartID(-1), RoomEndID(-1), ConnectionStatus(EConnectionStatus::DoorClosed) {}
 
     float GetConnectionStrength() const
     {
-        switch (DoorStatus)
+        switch (ConnectionStatus)
         {
-            case EDoorStatus::Closed:
+            case EConnectionStatus::DoorClosed:
                 return 0.8f;
-            case EDoorStatus::Open:
+            case EConnectionStatus::DoorOpen:
                 return 0.6f;
-            case EDoorStatus::NoDoor:
+            case EConnectionStatus::NoDoor:
                 return 0.4f;
             default:
-                return 1.0f;
+                return 0;
         }
     }
+};
+
+USTRUCT(BlueprintType)
+struct FGraphEdgeArray
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Building Graph")
+    TArray<FGraphEdge> Edges;
 };
 
 UCLASS()
@@ -70,21 +181,48 @@ class UBuildingGraph : public UObject
 
 public:
     UFUNCTION(BlueprintCallable, Category="Building Graph")
-    void InitializeGraph();
+    void PrepareGraphToWork();
 
     UFUNCTION(BlueprintCallable, Category="Building Graph")
-    void AddRoom(const FRoomNode& Room);
+    void AddRoom(const URoomNode* Room);
 
     UFUNCTION(BlueprintCallable, Category="Building Graph")
-    void AddDoor(const FDoorEdge& Door);
+    void AddConnection(const URoomNode* StartRoom, const URoomNode* EndRoom, EConnectionStatus ConnectionStatus);
 
-    UFUNCTION(BlueprintCallable, Category="Building Graph")
-    void CalculateRoomAreas();
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    const TMap<int32, URoomNode*>& GetRooms() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    void CalculateFireDynamicsForSecond(float TimeInSeconds, float TimeStep = 1.0f);
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    void MergeToSourceRoom(int32 TargetRoomID);
 
 protected:
-    UPROPERTY(VisibleAnywhere, Category="Building Graph")
-    TArray<FRoomNode> Rooms;
+    UPROPERTY(VisibleAnywhere, Category = "Building Graph")
+    TMap<int32, URoomNode*> Rooms;
 
-    UPROPERTY(VisibleAnywhere, Category="Building Graph")
-    TArray<FDoorEdge> Doors;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Building Graph")
+    TMap<int32, FGraphEdgeArray> OutgoingConnections;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Building Graph")
+    TMap<int32, FGraphEdgeArray> IncomingConnections;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Building Graph")
+    int32 SourceRoomID;
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    int32 FindSourceRoomId();
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    FFireDynamicsParameters CalculateFireDynamicsForRoom(URoomNode* Room, float CurrentTime);
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    void TopologicalSortUtil(int32 RoomID, TMap<int32, bool>& Visited, TArray<int32>& Stack);
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    void TopologicalSort();
+
+    UFUNCTION(BlueprintCallable, Category = "Building Graph")
+    void UpdateGraphConnectionsAfterMergeToSourceRoom(int32 TargetRoomID);
 };
